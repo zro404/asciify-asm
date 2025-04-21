f5a4d674a4ab434099741e376bc3451cf2b2a0f2
Harish G <dev.harish3124@gmail.com>
2025-04-21 19:28:22 +0530

    Add README

--- src/main.asm
+++ src/main.asm
@@ -2,12 +2,27 @@ format elf64 executable
 entry main
 
 include "io.inc"
+include "file.inc"
 
-hw:
-    db "Hello, World!", 0ah
+img:
+    db 0
+
+buffer:
+    times 256 db 0
 
 main:
-    print hw, 14
+    ;; Read File
+    mov rax, 2
+    mov rdi, [rsp+16]
+    mov rsi, 0
+    mov rdx, 0
+    syscall
+
+    mov [img], rax
+
+
+    fread img, buffer
+    print buffer, 20
 
 exit:
     mov rax, 60
